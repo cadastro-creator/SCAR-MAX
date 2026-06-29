832f61429622c251acdac7cbaba8ad05ee5e1e55
cadastro-creator <cadastro@contattos.com>
2026-06-29 09:38:39 -0300

    fix: variáveis de ambiente no workflow + renomear para NEXUS

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBztKU5tmt2yYCy5N0ABIjVwG9kuSXz_HU",
-  authDomain: "scar-max.firebaseapp.com",
-  projectId: "scar-max",
-  storageBucket: "scar-max.firebasestorage.app",
-  messagingSenderId: "677519848580",
-  appId: "1:677519848580:web:c807a911630b3747fdbed1"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
